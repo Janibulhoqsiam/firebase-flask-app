bc77febe90a9e51b13ef23637953ed250d6b3f83
Janibulhoqsiam <janibulhoquesyeam@gmail.com>
2024-12-23 22:38:42 +0600

    json changed

--- app.py
+++ app.py
@@ -102,20 +102,21 @@ def api_data():
         # Sample data
         data = [
             {
-                "id": 9,
-                "invaite_user": "51318627688",
-                "my_invaite": "426124254112",
-                "changer": 7,
-                "package": "com.india.diuwinhack3294",
-                "persent": 30,
-                "user_telegram": "https://t.me/arjun_earn",
-                "my_telegram": "@arjun_earn",
-                "AppVersion": 1,
-                "working_link": "https://diuwinapp.club",
-                "start": 1,
-                "login_url": "https://firebase-flask-app.onrender.com/diuwin2.0/login.php?number=",
-                "register_url": "https://pesagyan.com/diuwin2.0/register.php?number=",
-                "appName": "Diuwin Manish"
+
+                "id":9,
+                "invaite_user":"51318627688",
+                "my_invaite":"426124254112",
+                "changer":4,
+                "package":"com.india.diuwinhack3294",
+                "persent":30,
+                "user_telegram":"https:\/\/t.me\/arjun_earn",
+                "my_telegram":"@arjun_earn",
+                "AppVersion":1,
+                "working_link":"https:\/\/diuwin.bet",
+                "start":1,
+                "login_url":"https:\/\/pesagyan.com\/diuwin2.0\/login.php?number=",
+                "register_url":"https:\/\/pesagyan.com\/diuwin2.0\/register.php?number=",
+                "appName":"Diuwin Manish"
             }
         ]
 
